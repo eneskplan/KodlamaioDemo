8b7bb1fa4aec67540989d259712a9a6a7416d3db
ASUS <ASUS@LAPTOP-M3OLATMM>
2024-02-20 00:42:08 +0300

    Add project files.

---
+++ KodlamaioDemo/Entities/Concrete/Instructor.cs
@@ -0,0 +1,14 @@
+﻿using KodlamaioDemo.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaioDemo.Entities.Concrete
+{
+    internal class Instructor:IEntity
+    {
+
+    }
+}
